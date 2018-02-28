5dcff5d5be4ecc88e90cee8f11184821c6c7e7f3
tuechki <dtuechki@gmail.com>
2018-02-28 08:50:36 +0200

    package com.elsys.easybooker.repositories;

--- src/main/java/com/elsys/easybooker/repositories/BookingRecordRepository.java
+++ src/main/java/com/elsys/easybooker/repositories/BookingRecordRepository.java
@@ -3,5 +3,11 @@ package com.elsys.easybooker.repositories;
 import com.elsys.easybooker.models.BookingRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRecordRepository extends JpaRepository<BookingRecord, Long> {
+    public List<BookingRecord> findAllByUserId(long userId);
+    public List<BookingRecord> findAllByLocationId(long locationId);
+    public List<BookingRecord> findAllByServiceId(long serviceId);
+    public List<BookingRecord> findAllByUserIdAndLocationIdAndServiceId(long userId, long locationId, long serviceId);
 }
---
+++ src/main/java/com/elsys/easybooker/repositories/DayScheduleRepository.java
@@ -0,0 +1,11 @@
+package com.elsys.easybooker.repositories;
+
+import com.elsys.easybooker.models.DaySchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DayScheduleRepository extends JpaRepository<DaySchedule, Long>{
+    public List<DaySchedule> findAllByLocationId(long locationId);
+    public List<DaySchedule> findAllByLocationIdAndDayOfWeek(long locationId, int dayOfWeek);
+}
