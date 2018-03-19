484aa0d4542931b063fbb2f296048eb4f15258b2
tuechki <dtuechki@gmail.com>
2018-03-19 23:09:21 +0200

    Update services

--- src/main/java/com/elsys/easybooker/services/UserService.java
+++ src/main/java/com/elsys/easybooker/services/UserService.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class UserService {
+public class    UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
